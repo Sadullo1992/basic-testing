9e0eff7afb64ef9edb231a2b5cb1cad23f365ab3
Sadullo <sadulloburiyev1992@gmail.com>
2024-02-11 18:24:53 +0500

    feat: add snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const tree = generateLinkedList([
+      '<a href="https://nodejs.org">Nodejs</a>',
+      '<a href="http://www.twitter.com">Twitter</a>',
+    ]);
+    expect(tree).toStrictEqual({
+      next: {
+        next: {
+          next: null,
+          value: null,
+        },
+        value: '<a href="http://www.twitter.com">Twitter</a>',
+      },
+      value: '<a href="https://nodejs.org">Nodejs</a>',
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const tree = generateLinkedList([
+      '<a href="http://www.facebook.com">Facebook</a>',
+      '<a href="http://www.instagram.com">Instagram</a>',
+    ]);
+    expect(tree).toMatchSnapshot();
   });
 });
